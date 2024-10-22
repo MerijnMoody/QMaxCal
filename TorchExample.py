9e7d1ad457f3f2141e82ac11453e85989fff33ff
MerijnMoody <77996738+MerijnMoody@users.noreply.github.com>
2024-10-22 23:03:25 +0200

    Update TorchExample.py

--- TorchExample.py
+++ TorchExample.py
@@ -23,13 +23,14 @@ evo_time = 1
 times = np.linspace(0,evo_time,n_ts)
 c1 = -2
 c2 = -3
+c3 = 0.2
 
-L0 = liouvillian(H_sys, c_ops=[c1*a, c2*a*a.dag()])
+L0 = liouvillian(H_sys, c_ops=[c1*a, c2*a*a.dag(), c3*(a+a.dag())])
 
 rho0 = operator_to_vector(Qobj([[0.8,0],[0,0.2]]))
 newr = vector_to_operator(rho0)
 rhotar = operator_to_vector(Qobj([[0.1,0],[0,0.9]]))
-measurements = [[to_super(Qobj([[1, 0],[0, 0]])).full(), to_super(Qobj([[0,0],[0,1]])).full()] ,[1, 2, 3]]
+measurements = [[to_super(Qobj([[1, 0],[0, 0]])).full(), to_super(Qobj([[0,0],[0,1]])).full()] ,[1, 15, 30]]
 
 othertimes = np.linspace(0, evo_time, n_ts)
 othertimes = othertimes.tolist()
@@ -47,10 +48,10 @@ plt.plot(othertimes, onelist, color='blue',  linestyle='dashed', markersize=3)
 
 time_list = np.linspace(0, evo_time, n_ts)[:-1]
 
-# ctrls = torch.load('databigrun.pth')['ctrls']
+# = torch.load('databigrun.pth')['ctrls']
 ctrls = None
 evolution = create_evolution(L0, H_con, Ham_list, rho0, rhotar, ref_evo, time_list, glob_dim, ctrls, measurements)
-ctrls = evolution.optimize(1000, 0.000005, 0, 0)
+ctrls = evolution.optimize(10000, 0.005, 0, 0)
 
 print("lam:")
 print(evolution.lam)
